b29bb11063847020e111994344b5bb80dcebdd8b
Jonathan <jonarodriguez91@gmail.com>
2026-09-16 19:04:07 -0300

    Audio Sliders
    The sliders were adjusted to better fit the screen size. A volume indicator was added for each slider, and the value was also added.

--- Assets/Scripts/UI/UI Settings/PanelVolume.cs
+++ Assets/Scripts/UI/UI Settings/PanelVolume.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
@@ -6,9 +7,13 @@ public class PanelVolume : MonoBehaviour
 {
     [SerializeField] private AudioMixer mixer;
     [SerializeField] private Slider sliderMaster;
+    [SerializeField] private TMP_Text textMaster;
     [SerializeField] private Slider sliderBackgroud;
+    [SerializeField] private TMP_Text textBackgroud;
     [SerializeField] private Slider sliderSfx;
+    [SerializeField] private TMP_Text textSfx;
     [SerializeField] private Slider sliderUi;
+    [SerializeField] private TMP_Text textUi;
 
     private void Awake()
     {
@@ -23,15 +28,19 @@ public class PanelVolume : MonoBehaviour
         float volume = 0;
         mixer.GetFloat("VolumeMaster", out volume);
         sliderMaster.value = volume;
+        textMaster.text = sliderMaster.value.ToString("F2");
 
         mixer.GetFloat("VolumeBackgroud", out volume);
         sliderBackgroud.value = volume;
+        textBackgroud.text = sliderBackgroud.value.ToString("F2");
 
         mixer.GetFloat("VolumeSFX", out volume);
         sliderSfx.value = volume;
+        textSfx.text = sliderSfx.value.ToString("F2");
 
         mixer.GetFloat("VolumeUI", out volume);
         sliderUi.value = volume;
+        textUi.text = sliderUi.value.ToString("F2");
     }
 
     private void OnDestroy()
@@ -42,10 +51,29 @@ public class PanelVolume : MonoBehaviour
         sliderUi.onValueChanged.RemoveAllListeners();
     }
 
-    private void OnSliderMasterChanged(float value) => ModifyMixerVolume("VolumeMaster", value);
-    private void OnsliderBackgroudChanged(float value) => ModifyMixerVolume("VolumeBackgroud", value);
-    private void OnSliderSfxChanged(float value) => ModifyMixerVolume("VolumeSFX", value);
-    private void OnSliderUiChanged(float value) => ModifyMixerVolume("VolumeUI", value);
+    private void OnSliderMasterChanged(float value)
+    {
+        ModifyMixerVolume("VolumeMaster", value);
+        textMaster.text = value.ToString("F2");
+    }
+
+    private void OnsliderBackgroudChanged(float value)
+    {
+        ModifyMixerVolume("VolumeBackgroud", value);
+        textBackgroud.text = value.ToString("F2");
+    }
+
+    private void OnSliderSfxChanged(float value)
+    {
+        ModifyMixerVolume("VolumeSFX", value);
+        textSfx.text = value.ToString("F2");
+    }
+
+    private void OnSliderUiChanged(float value)
+    {
+        ModifyMixerVolume("VolumeUI", value);
+        textUi.text = value.ToString("F2");
+    }
 
     private void ModifyMixerVolume(string parameter, float volume)
     {
